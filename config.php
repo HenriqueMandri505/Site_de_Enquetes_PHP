0e467830bcf32e858caea10daae865ca9d48fadf
HenriqueMandri <107136785+HenriqueMandri505@users.noreply.github.com>
2026-02-05 19:38:40 -0300

    Change database configuration to use SQLite

--- config.php
+++ config.php
@@ -2,12 +2,19 @@
 
 //configuração de bd
 
-define("DB_DSN", "mysql:host=localhost;dbname=polls;charset=utf8mb4");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define('DB_DSN', 'sqlite:' . __DIR__ . '/database.db');
+define('DB_USER', null);
+define('DB_PASS', null);
+
+// USO LOCAL
+
+// define("DB_DSN", "mysql:host=localhost;dbname=polls;charset=utf8mb4");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
 
 //cookies para a votação
 define("VOTE_COOKIE_DAYS", 30);
+
 
 
 
